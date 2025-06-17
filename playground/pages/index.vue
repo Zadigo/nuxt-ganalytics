570fa5b43b52984087bb3a3cefd417ee9f3bdda9
Zadigo <pendenquejohn@gmail.com>
2025-06-18 01:06:02 +0200

    Define icons

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="site" class="px-20">
+  <section id="site">
     <NuxtContainer>
       <div class="max-w-xl mx-auto my-20">
         <NuxtCard class="border-0 shadow-md">
@@ -13,12 +13,30 @@
                 </NuxtBadge>
                 
                 <div class="my-5">
+                  <div class="flex justify-between my-3">
+                    <NuxtInput v-model="search" placeholder="Search by event name..." />
+                    
+                    <div class="flex justify-around gap-2">
+                      <NuxtButton color="secondary" @click="() => { displayObjects = false }">
+                        <Icon name="i-lucide-letter-text" />
+                      </NuxtButton>
+
+                      <NuxtButton color="secondary" @click="() => { displayObjects = true }">
+                        <Icon name="lucide:file-json-2" />
+                      </NuxtButton>
+                    </div>
+                  </div>
+                  
                   <p class="font-bold">Datalayer</p>
 
-                  <NuxtInput v-model="search" placeholder="Search by event name..." />
                   <div class="p-5 h-[300px] overflow-y-scroll space-y-1">
-                    <div v-for="(item, i) in filteredEvents" :key="i" class="p-5 bg-secondary-50 rounded-md">
-                      {{ item }}
+                    <div v-for="(item, i) in filteredEvents" :key="i" class="p-5 bg-gray-100 rounded-md">
+                      <span v-if="displayObjects">{{ item }}</span>
+                      <div v-else>
+                        <NuxtBadge :icon="getEventIcon(item.event)" color="info">
+                          {{ item.event }}
+                        </NuxtBadge>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -44,8 +62,18 @@ interface CustomDataLayerObject extends DataLayerObject {
   target?: string | null
 }
 
+const eventNames = [
+  'gtm.js',
+  'gtm.load',
+  'gtm.dom',
+  'gtm.click'
+] as const
+
+type EventNames = (typeof eventNames)[number]
+
 const gtm = useGtm()
 
+const displayObjects = ref<boolean>(false)
 const search = ref<string>('')
 
 const isEnabled = computed(() => {
@@ -78,6 +106,22 @@ const filteredEvents = computed(() => {
 function handleTestEvent() {
   if (gtm) {
     gtm.trackEvent({ event: 'Test click', action: 'click', value: 'Nuxt' })
+  }
+}
+
+function getEventIcon(name: EventNames) {
+  const icons = {
+    'gtm.js': 'lucide:code-xml',
+    'gtm.dom': 'lucide:code',
+    'gtm.load': 'lucide:loader',
+    'gtm.click': 'lucide:mouse-pointer-click'
+  }
+  const icon = icons[name]
+
+  if (icon) {
+    return icon
+  } else {
+    return 'lucide:loader'
   }
 }
 </script>
