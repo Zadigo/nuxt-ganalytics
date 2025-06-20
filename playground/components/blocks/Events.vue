9296a4eaa13ac0febe75453c70c946467b1407db
Zadigo <pendenquejohn@gmail.com>
2025-06-20 13:14:17 +0200

    Create specific events button

---
+++ playground/components/blocks/Events.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="py-5 space-x-2 flex">
+    <NuxtAnalytics event="search" :params="{ search_term: 'My application' }">
+      <template #default="{ attrs }">
+        <NuxtButton @click="attrs.sendTemplateEvent()">
+          Login
+        </NuxtButton>
+      </template>
+    </NuxtAnalytics>
+  </div>
+</template>
