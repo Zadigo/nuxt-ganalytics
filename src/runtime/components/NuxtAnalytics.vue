cb0eb48ffb644fca81b27e3609a89c121d858874
Zadigo <pendenquejohn@gmail.com>
2025-06-20 13:14:41 +0200

    Emit events back to parent

--- src/runtime/components/NuxtAnalytics.vue
+++ src/runtime/components/NuxtAnalytics.vue
@@ -9,7 +9,7 @@
 <script setup lang="ts">
 import { defineEvent, useAnalyticsEvent } from '#imports'
 import { computed, useTemplateRef } from 'vue'
-import type { EventNames, EventParameters } from '../types'
+import type { EventNames, EventParameters, GAnalyticsDatalayerObject } from '../types'
 
 interface Props {
   event: EventNames
@@ -18,13 +18,15 @@ interface Props {
 
 const ganalyticsEl = useTemplateRef('ganalyticsEl')
 const props = defineProps<Props>()
+const emit = defineEmits<{ 'ga-event': [ data: GAnalyticsDatalayerObject | undefined ] }>()
 
 const { sendEvent } = useAnalyticsEvent()
 
 const defaultAttrs = computed(() => {
   return {
     sendTemplateEvent () {
-      sendEvent(defineEvent(props.event, props.params))
+      const parsedResult = sendEvent(defineEvent(props.event, props.params))
+      emit('ga-event', parsedResult)
     }
   }
 })
